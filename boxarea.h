1ce720ea5a83208bd9015fb5b5611cb9a02cbf21
0139450 <159297417+0139450@users.noreply.github.com>
2024-02-15 10:27:51 +0530

    Create boxarea.h

---
+++ boxarea.h
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void boxArea(float length,float height,float width)
+{
+    /*cout<<"enetr the length"<<endl;
+    cin>>length;
+    cout<<"enetr the height"<<endl;
+    cin>>height;
+    cout<<"enter the width"<<endl;
+    cin>>width;
+    cout<<"calculating"<<endl;*/
+    cout<<"area: "<<2*length*height+2*length*width+2*height*width<<endl;
+
+}
