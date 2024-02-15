169b067c6dbabc6e11dca7e42359734b011ef60a
0139450 <159297417+0139450@users.noreply.github.com>
2024-02-15 10:28:30 +0530

    Create boxvolume.h

---
+++ boxvolume.h
@@ -0,0 +1,15 @@
+#include<iostream>
+/*#ifndef length
+#define length 5
+#endif*/
+using namespace std;
+void boxVolume(float length,float height,float width)
+{
+   /* cout<<"enetr the length"<<endl;
+    cin>>length;
+    cout<<"enetr the height"<<endl;
+    cin>>height;
+    cout<<"enter the width"<<endl;
+    cin>>width;*/
+    cout<<"volume: "<<length*width*height<<endl;
+}    
